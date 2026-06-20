7624470ade54f22e43de71e2b72daa2697bda0b5
eiwashita <erikoiwashita@gmail.com>
2026-06-19 18:39:33 -0600

    Add CORS settings

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -4,9 +4,18 @@ using GameStore.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 var connString = builder.Configuration.GetConnectionString("GameStore");
+
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowAll", policy =>
+    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
